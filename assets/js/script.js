5bf84ccc8e8fead70b63d824f0aa0b432d08be53
chadkraus87 <127354484+chadkraus87@users.noreply.github.com>
2023-04-14 07:40:33 -0500

    Added live clock to display current hour, minute, seconds.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,46 @@
+// $(document).ready(function() {
+//   // Listen for click events on the save button.
+//   $(".saveBtn").on("click", function() {
+//     // Save the user input in local storage using the id in the time block.
+//     var key = $(this).parent().attr("id");
+//     var value = $(this).siblings(".description").val();
+//     localStorage.setItem(key, value);
+//   });
+
+//   // Compare the id to the current hour and apply the appropriate past, present, or future class to each hour block.
+//   function updateHourlyStatus() {
+//     var currentHour = moment().hours();
+//     $(".time-block").each(function() {
+//       var blockHour = parseInt($(this).attr("id").split("-")[1]);
+//       if (blockHour < currentHour) {
+//         $(this).addClass("past");
+//       } else if (blockHour === currentHour) {
+//         $(this).removeClass("past");
+//         $(this).addClass("present");
+//       } else {
+//         $(this).removeClass("past");
+//         $(this).removeClass("present");
+//         $(this).addClass("future");
+//       }
+//     });
+//   }
+//   updateHourlyStatus();
+
+//   // Pull any user input that was saved in the localStorage and set the values of the corresponding textarea elements.
+//   $(".description").each(function() {
+//     var key = $(this).parent().attr("id");
+//     var value = localStorage.getItem(key);
+
+//     if (value) {
+//       $(this).val(value);
+//     }
+//   });
+
+//   // Show the current date and time on the page.
+//   $("#currentDay").text(moment().format("dddd, MMMM Do"));
+// });
+
+
 $(document).ready(function() {
   // Listen for click events on the save button.
   $(".saveBtn").on("click", function() {
@@ -38,4 +81,12 @@ $(document).ready(function() {
 
   // Show the current date and time on the page.
   $("#currentDay").text(moment().format("dddd, MMMM Do"));
+
+  // Show the current time on the page.
+  function updateTime() {
+    var currentTime = moment().format("h:mm:ss a");
+    $("#currentTime").text(currentTime);
+  }
+
+  setInterval(updateTime, 1000);
 });
